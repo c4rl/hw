b0b286bebab3b692f30f920142d5b2c4acfeabc8
Carl Wiedemann <carl.wiedemann@gmail.com>
2016-12-03 14:22:36 -0700

    select muppets

--- php/muppetrest/index.php
+++ php/muppetrest/index.php
@@ -2,6 +2,8 @@
 
 require 'vendor/autoload.php';
 
+define('WEBROOT', __DIR__);
+
 main();
 
 function main() {
@@ -20,7 +22,7 @@ function main() {
       $content = NULL;
   }
 
-  print json_encode($content);
+  print json_encode($content, JSON_PRETTY_PRINT);
 
 }
 
@@ -30,4 +32,10 @@ function qs() {
 
 function route() {
   return trim(parse_url($_SERVER['REQUEST_URI'])['path'], '/');
+}
+
+function filter_string_keys(array $row) {
+  return array_filter($row, function ($value, $key) {
+    return !is_numeric($key);
+  }, ARRAY_FILTER_USE_BOTH);
 }
--- php/muppetrest/model/Muppet.php
+++ php/muppetrest/model/Muppet.php
@@ -5,9 +5,22 @@ namespace Muppet;
 class Muppet {
 
   public static function all() {
-    return [
-      'errbody',
-    ];
+
+    $pdo = new \PDO(sprintf('sqlite:%s/storage/db.sqlite', WEBROOT));
+
+    $page = 1;
+    $per_page = 10;
+
+    $result = $pdo->query(sprintf('SELECT rowid,* FROM muppets LIMIT %d, %d', $page - 1, $per_page));
+
+    $total = 0;
+    $muppets = [];
+    foreach ($result as $row) {
+      $total++;
+      $muppets[] = filter_string_keys($row);
+    }
+
+    return compact('total', 'page', 'per_page', 'muppets');
   }
 
 }
