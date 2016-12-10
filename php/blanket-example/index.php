f48d61f39a5aa3859857c28fb8c6b8508b6867db
Carl Wiedemann <carl.wiedemann@gmail.com>
2016-12-10 16:17:21 -0700

    clean-up for big example

--- php/blanket-example/index.php
+++ php/blanket-example/index.php
@@ -1,20 +1,10 @@
 <?php
 
 use Blanket\App;
-use Blanket\Db\Db;
 use Blanket\Request;
-
-use Muppet\Muppet;
 
 require 'vendor/autoload.php';
 
-$config = [
-  'resources' => [
-    'muppets' => Muppet::class,
-  ],
-  'storage' => (new Db(sprintf('sqlite:%s/storage/db.sqlite', __DIR__))),
-];
-
-$app = new App($config);
+$app = new App();
 
 $app->run(Request::createFromGlobals());
